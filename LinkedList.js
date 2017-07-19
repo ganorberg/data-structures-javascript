bc8c742a96166a9c93b366998022a9a484cf81b1
George Norberg <george.norberg@gmail.com>
2017-07-19 14:04:48 -0700

    everybody loves linked lists

---
+++ LinkedList.js
@@ -0,0 +1,143 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  /* Add nodes with given value to instance of LinkedList.
+
+  Strategy: Since we have access to tail, append newly created Node to tail and move tail pointer to new node.
+  
+  Edge case: empty list
+
+  @param {*} value - value to be inserted into new node
+  @return {bool|string} - informs user that push was successful | edge case message
+  */
+  push(value) {
+    if (value === null || value === undefined) {
+      return 'Please don\'t push silly values :)';
+    }
+
+    const node = new Node(value);
+    this.size++;
+
+    // Edge case: empty list
+    if (this.head === null) {
+      this.head = this.tail = node;
+      return true;
+    }
+    
+    this.tail.next = node;
+    this.tail = node;
+    return true;
+  }
+
+  /* Check if node with given value exists in instance of LinkedList.
+
+  Strategy: Loop through LL checking if value of node equals input value. End loop when we reach the last node based on node's next value.
+  
+  Edge case: empty list
+
+  @param {*} value - checked if exists in list
+  @return {bool|string} - whether or not value exists in list | edge case message
+  */  
+  contains(value) {
+    // Edge case: empty list
+    if (this.size === 0) { return 'list is empty'; }
+
+    let curr = this.head;
+    while (curr !== null) {
+      if (curr.value === value) { return true; }
+      curr = curr.next;
+    }
+
+    return false;
+  }
+
+  /* Remove nodes with given value from instance of LinkedList.
+
+  Strategy: Loop through LL tracking previous and current nodes so you can remove reference to target node by pointing prev's next at current's next. Only removes first match.
+  
+  Edge cases: empty list, remove head or tail, removing final node.
+
+  @param {*} value - value to be removed from list
+  @return {object|string} - node removed | edge case message
+  */
+  remove(value) {
+    // Edge case: empty list
+    if (this.size === 0) { return 'list already empty'; }
+
+    // Edge case: if head matches, need to update head
+    if (this.head.value === value) {
+      const node = this.head;
+      this.head = this.head.next;
+
+      // Edge case: if removing final node in list
+      if (this.size === 1) { this.tail = null; }
+
+      this.size--;
+      return node;
+    }
+
+    let prev = this.head;
+    let curr = this.head.next;
+
+    while (curr !== null) {
+      if (curr.value === value) {
+
+        // Edge case: if tail matches, need to update tail
+        if (curr === this.tail) { this.tail = prev; }
+
+        const node = curr;
+        prev.next = curr.next;
+        this.size--;
+        return node;
+      }
+
+      prev = curr;
+      curr = curr.next;
+    }
+
+    return 'Value does not exist in list';
+  }
+}
+
+
+const list = new LinkedList();
+list.push(1);
+list.push(2);
+console.log(list.contains(2));
+list.push(3);
+list.remove(2);
+console.log(list.contains(2));
+console.log(list);
+
+const list2 = new LinkedList();
+list2.push('I am alone');
+list2.remove('I am alone');
+console.log(list2);
+
+const list3 = new LinkedList();
+list3.push(1);
+list3.push('I will survive');
+list3.remove(1);
+console.log(list3);
+
+const list4 = new LinkedList();
+list4.push('I will survive');
+list4.push(2);
+list4.remove(2);
+console.log(list4);
+
+const notRemoved = new LinkedList();
+notRemoved.push(1);
+console.log(notRemoved.remove('Does not exist'));
+console.log(notRemoved);
