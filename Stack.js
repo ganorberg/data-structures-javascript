945c07d91fdd78cd10d808ea23940a0b7b221a19
George Norberg <george.norberg@gmail.com>
2017-07-26 03:30:16 -0700

    add Stack data structure with isEmpty, isFull, peek, push, pop and size methods

---
+++ Stack.js
@@ -0,0 +1,93 @@
+class Stack {
+  /**
+   * Represents a stack
+   * @constructor
+   * @param {Number=} capacity 
+   */
+  constructor(capacity = Infinity) {
+    this.storage = [];
+    this.capacity = capacity;
+  }
+
+  /** 
+   * Description: Check if stack contains any values
+   * Strategy: Use storage array length
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {Boolean} - true if empty, or false otherwise
+   */
+  isEmpty() {
+    return this.storage.length === 0;
+  }
+
+  /** 
+   * Description: Check if stack has reached capacity
+   * Strategy: Check if storage array length matches capacity value
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {Boolean} - true if full, or false otherwise
+   */
+  isFull() {
+    return this.storage.length === this.capacity;
+  }
+
+  /**
+   * Description: View value at top of stack
+   * Strategy: Look at storage array's final element using index
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {*} - value at top of stack
+   */
+  peek() {
+    return this.storage[this.storage.length - 1];
+  }
+
+  /**
+   * Description: Add input value to top of stack
+   * Strategy: Use native Array push method if under capacity
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @param {*} val - value added to stack
+   * @return {Number} - size of stack after value inserted
+   */
+  push(val) {
+    if (this.storage.length < this.capacity) { this.storage.push(val); }
+    return this.storage.length;
+  }
+
+  /**
+   * Description: Remove value from top of stack
+   * Strategy: Use native Array pop method
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {*} - value removed from stack, or undefined if empty
+   */
+  pop() {
+    return this.storage.pop();
+  }
+
+  /**
+   * Description: Check size of stack
+   * Strategy: Use storage length
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {Number} - total amount of items in stack
+   */
+  size() {
+    return this.storage.length;
+  }
+}
+
+module.exports = Stack;
