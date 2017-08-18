fc0d183750e881936b1bcc9376dc33caa6227684
George Norberg <george.norberg@gmail.com>
2017-08-18 14:41:29 -0700

    Implement getMax
    ... such that getMax, push and pop all operate in constant time.

--- structures/Stack.js
+++ structures/Stack.js
@@ -1,12 +1,27 @@
 class Stack {
   /**
-   * Represents a stack
+   * Represents a stack for numbers
    * @constructor
    * @param {Number=} capacity 
    */
   constructor(capacity = Infinity) {
+    this.maxes = [-Infinity];
     this.storage = [];
     this.capacity = capacity;
+  }
+
+  /** 
+   * @description Check maximum in stack
+   * 
+   * Strategy: Return last value added to maxes array
+   *
+   * Time complexity: O(1)
+   * Space complexity: O(1)
+   *
+   * @return {Number} - maximum value in stack
+   */
+  getMax() {
+    return this.maxes[this.maxes.length - 1];
   }
 
   /** 
@@ -54,7 +69,8 @@ class Stack {
   /**
    * @description Add input value to top of stack
    * 
-   * Strategy: Use native Array push method if under capacity
+   * Strategy: Use native Array push method if under capacity. If value is
+   * greater than previous max, make it the maximum.
    *
    * Time complexity: O(1)
    * Space complexity: O(1)
@@ -63,21 +79,28 @@ class Stack {
    * @return {Number} - size of stack after value inserted
    */
   push(val) {
-    if (this.storage.length < this.capacity) { this.storage.push(val); }
+    if (this.storage.length < this.capacity) {
+      this.storage.push(val);
+      if (val > this.maxes[this.maxes.length - 1]) { this.maxes.push(val); }
+    }
+
     return this.storage.length;
   }
 
   /**
    * @description Remove value from top of stack
    * 
-   * Strategy: Use native Array pop method
+   * Strategy: Use native Array pop method. Check if maximum for removal.
    *
    * Time complexity: O(1)
    * Space complexity: O(1)
    *
-   * @return {*} - value removed from stack, or undefined if empty
+   * @return {Number} - value removed from stack, or undefined if empty
    */
   pop() {
+    const popped = this.storage[this.storage.length - 1];
+    const max = this.maxes[this.maxes.length - 1];
+    if (popped === max) { this.maxes.pop(); }
     return this.storage.pop();
   }
 
