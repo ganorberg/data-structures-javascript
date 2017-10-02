51ebc84ded99c474aac066f36dbdacc895032aea
George Norberg <george.norberg@gmail.com>
2017-10-01 21:25:30 -0700

    Create tests for DepthFirstPaths graph processor

---
+++ test/graphs/processors/BreadthFirstPaths.js
@@ -0,0 +1,37 @@
+
+// const graph = new UndirectedGraph();
+
+// const edges = [
+//   [0, 5],
+//   [2, 4],
+//   [2, 3],
+//   [1, 2],
+//   [0, 1],
+//   [3, 4],
+//   [3, 5],
+//   [0, 2],
+//   [0, 5],
+//   [4, 3],
+//   [0, 1],
+//   [9, 12],
+//   [6, 4],
+//   [5, 4],
+//   [0, 2],
+//   [11, 12],
+//   [9, 10],
+//   [0, 6],
+//   [7, 8],
+//   [9, 11],
+//   [5, 3],
+//   [5, 5],
+//   [14, 14],
+// ];
+
+// edges.forEach(edge => graph.addEdge(edge));
+
+// const bfPaths = new BreadthFirstPaths(graph, 0);
+// bfPaths.initialize();
+// console.log(bfPaths);
+// console.log(graph.adjacencyList);
+// console.log(bfPaths.shortestPathTo(6));
+// console.log(bfPaths.distanceTo(4));
---
+++ test/graphs/processors/ConnectedComponents.js
@@ -0,0 +1,26 @@
+// const graph = new UndirectedGraph();
+
+// const edges = [
+//   [0, 5],
+//   [4, 3],
+//   [0, 1],
+//   [9, 12],
+//   [6, 4],
+//   [5, 4],
+//   [0, 2],
+//   [11, 12],
+//   [9, 10],
+//   [0, 6],
+//   [7, 8],
+//   [9, 11],
+//   [5, 3],
+//   [5, 5],
+//   [14, 14],
+// ];
+
+// edges.forEach(edge => graph.addEdge(edge));
+
+// const cc = new ConnectedComponents(graph, 0);
+// cc.initialize();
+// console.log(cc);
+// console.log('component id: ', cc.componentId(12));
